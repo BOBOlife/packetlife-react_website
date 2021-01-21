a23d6cfd34fffc6841a68a7f7adeb53d535ba009
wangbo <bbw1996@qq.com>
2021-01-22 02:11:55 +0800

    deploy

---
+++ precache-manifest.4871248bd77e6faef6c0cb462015ad4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86069b929d1c8298a300ea195a41c9b9",
+    "url": "/packetlife-react_website/index.html"
+  },
+  {
+    "revision": "d4a2dd33761ba6ae7953",
+    "url": "/packetlife-react_website/static/css/main.8564a6ec.chunk.css"
+  },
+  {
+    "revision": "c50cf4cdd3fe2a08aaed",
+    "url": "/packetlife-react_website/static/js/2.db8f6f25.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/packetlife-react_website/static/js/2.db8f6f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a2dd33761ba6ae7953",
+    "url": "/packetlife-react_website/static/js/main.46c75215.chunk.js"
+  },
+  {
+    "revision": "8a7095e2b8ab70353c20",
+    "url": "/packetlife-react_website/static/js/runtime-main.b043b1a0.js"
+  }
+]);
